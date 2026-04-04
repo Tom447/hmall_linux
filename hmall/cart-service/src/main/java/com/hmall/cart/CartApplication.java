d64f4f8c9dddab197200b10df971a8c6f5ce698c
xiaoma <1142322503@qq.com>
2026-04-04 17:43:47 +0800

    抽取cart-service中的feignclien的api到hm-api中 	实现feignclien公共的共同使用

--- hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.hmall.cart.mapper")
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.hmall.api.client")
 @SpringBootApplication
 public class CartApplication {
     public static void main(String[] args) {
