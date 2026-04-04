c97f9a21c3950c82633e8c46fb2ad44cef627842
xiaoma <1142322503@qq.com>
2026-04-04 15:04:34 +0800

    cart-service 调用item-service 手写实现远程调度和负载均衡

--- hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,22 +1,36 @@
 package com.hmall.cart.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmall.cart.domain.dto.CartFormDTO;
+import com.hmall.cart.domain.dto.ItemDTO;
 import com.hmall.cart.domain.po.Cart;
 import com.hmall.cart.domain.vo.CartVO;
 import com.hmall.cart.mapper.CartMapper;
 import com.hmall.cart.service.ICartService;
+import com.hmall.common.exception.BadRequestException;
 import com.hmall.common.exception.BizIllegalException;
 import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -30,7 +44,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
-
+    private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -67,17 +82,36 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         List<CartVO> vos = BeanUtils.copyList(carts, CartVO.class);
 
         // 3.处理VO中的商品信息
-//        handleCartItems(vos);
+        handleCartItems(vos);
 
         // 4.返回
         return vos;
     }
 
-   /* private void handleCartItems(List<CartVO> vos) {
-        //TODO  1.获取商品id
+    private void handleCartItems(List<CartVO> vos) {
+        // 1.获取商品id
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
-        List<ItemDTO> items = itemService.queryItemByIds(itemIds);
+        //2.1 获取服务实例列表
+        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+        //2.2 手写负载均衡
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+        //2.3 获取当前实例的ip和端口
+        URI uri = instance.getUri();
+        int port = instance.getPort();
+
+        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
+                instance.getUri() + "/items?ids={ids}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<ItemDTO>>() {
+                },
+                StrUtil.join(",", itemIds)
+        );
+        if(!response.getStatusCode().is1xxInformational()){
+            return;
+        }
+        List<ItemDTO> items = response.getBody();
         if (CollUtils.isEmpty(items)) {
             throw new BadRequestException("购物车中商品不存在！");
         }
@@ -93,7 +127,7 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
             v.setStatus(item.getStatus());
             v.setStock(item.getStock());
         }
-    }*/
+    }
 
     @Override
     public void removeByItemIds(Collection<Long> itemIds) {
