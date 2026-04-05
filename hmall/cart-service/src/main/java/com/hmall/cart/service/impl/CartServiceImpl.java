3f9d41e88b9908152b37cd70a18df6243fc5d4fa
xiaoma <1142322503@qq.com>
2026-04-05 22:28:37 +0800

    fix自动装配拦截器和普通服务于gateway下，有条件的使得拦截器生效

--- hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -68,6 +68,8 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     @Override
     public List<CartVO> queryMyCarts() {
         // 1.查询我的购物车列表
+
+        System.out.println(UserContext.getUser());
         List<Cart> carts = lambdaQuery().eq(Cart::getUserId, UserContext.getUser()).list();
         if (CollUtils.isEmpty(carts)) {
             return CollUtils.emptyList();
